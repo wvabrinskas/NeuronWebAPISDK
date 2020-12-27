15d4c1813863d80e5c2ee9b390d2332ddbb3217b
William Vabrinskas <wvabrinskas@gmail.com>
2020-12-27 18:48:34 -0500

    made public

--- Sources/NeuronWebAPISDK/Models/InitModel.swift
+++ Sources/NeuronWebAPISDK/Models/InitModel.swift
@@ -31,13 +31,13 @@ public struct Layer: Codable {
 }
 
 public struct InitModel: Codable {
-  var layers: [Layer]
-  var learningRate: Float
-  var bias: Float
-  var lossFunction: LossFunctionMode
-  var lossThreshold: Float
-  var modifier: ModifierMode? = nil
-  var epochs: Int
+  public var layers: [Layer]
+  public var learningRate: Float
+  public var bias: Float
+  public var lossFunction: LossFunctionMode
+  public var lossThreshold: Float
+  public var modifier: ModifierMode? = nil
+  public var epochs: Int
 
   public init(layers: [Layer],
               learningRate: Float,
